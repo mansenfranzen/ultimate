500f552f0fd56a4852b316b4667ae98e5c9f26d2
Franz Wöllert <franz.woellert@gmail.com>
2016-09-18 19:06:19 +0200

    Fix incorrect app name from 'ultimate2' to 'ultimate'.

--- ultimate/wsgi.py
+++ ultimate/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ultimate2 project.
+WSGI config for ultimate project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ultimate2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ultimate.settings")
 
 application = get_wsgi_application()
