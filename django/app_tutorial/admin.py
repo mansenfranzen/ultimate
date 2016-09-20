61460940206d43c9ed8ff9333e86111b2fd76de7
Franz Wöllert <franz.woellert@gmail.com>
2016-09-20 23:46:33 +0200

    Register models for admin site.

--- django/app_tutorial/admin.py
+++ django/app_tutorial/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Tournament, Team, TournamentParticipation, Game, \
+    GameReport, SpirtScore, Player
+
+admin.site.register(Player)
+admin.site.register(Team)
+admin.site.register(Tournament)
+admin.site.register(TournamentParticipation)
+admin.site.register(Game)
+admin.site.register(GameReport)
+admin.site.register(SpirtScore)
