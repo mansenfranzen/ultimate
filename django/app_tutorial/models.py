3560095b527b8dd0b8e77905ef3a89ac4ef94fcb
Franz Wöllert <franz.woellert@gmail.com>
2016-09-20 23:15:27 +0200

    Add Player, TournamentParticipation table defintions. Modify other definitions to comply with updated database design.

--- django/app_tutorial/models.py
+++ django/app_tutorial/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 
+class Player(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birthday = models.DateTimeField('Birthday')
+    nationality = models.IntegerField()
+    external_id_type = models.IntegerField()
+    external_id = models.IntegerField()
+
+
 class Team(models.Model):
     name = models.CharField(max_length=50)
     origin = models.CharField(max_length=50)
+    division = models.IntegerField()
 
 
 class Tournament(models.Model):
@@ -15,20 +25,34 @@ class Tournament(models.Model):
 
 
 class Game(models.Model):
-    tournament = models.ForeignKey(Tournament)
-    time_stamp = models.DateTimeField('Game Time')
+    tournament_id = models.ForeignKey(Tournament)
+    date = models.DateTimeField('Game Time')
+    team_id_home = models.ForeignKey(Team)
+    team_id_away = models.ForeignKey(Team)
+    score_home = models.IntegerField()
+    score_away = models.IntegerField()
     type = models.IntegerField()
+    round = models.IntegerField()
 
 
-class GameScore(models.Model):
-    game = models.ForeignKey(Game)
-    team = models.ForeignKey(Team)
-    value = models.IntegerField()
+class GameReport(models.Model):
+    game_id = models.ForeignKey(Game)
+    player_id_assist = models.ForeignKey(Player)
+    player_id_score = models.ForeignKey(Player)
+    timestamp = models.DateTimeField('Timestamp')
+    event = models.IntegerField()
 
 
 class SpirtScore(models.Model):
-    game = models.ForeignKey(Game)
-    subject = models.ForeignKey(Team)
-    object = models.ForeignKey(Team)
-    type = models.IntegerField()
-    value = models.IntegerField()
+    game_id = models.ForeignKey(Game)
+    team_id_providing = models.ForeignKey(Team)
+    team_id_receiving = models.ForeignKey(Team)
+    score_category = models.IntegerField()
+    score = models.IntegerField()
+
+
+class TournamentParticipation(models.Model):
+    tournament_id = models.ForeignKey(Tournament)
+    team_id = models.ForeignKey(Team)
+    player_id = models.ForeignKey(Player)
+    player_number = models.IntegerField()
