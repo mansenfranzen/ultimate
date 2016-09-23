a2782b600cafa90e82b59f9c9117dba94d706829
Franz Wöllert <franz.woellert@gmail.com>
2016-09-23 18:52:00 +0200

    Add new tables to admin site.

--- django/app_tutorial/admin.py
+++ django/app_tutorial/admin.py
@@ -3,12 +3,19 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Tournament, Team, TournamentParticipation, Game, \
-    GameReport, SpirtScore, Player
+    GameReport, SpirtScore, Profile, Nationality, Division, TournamentType, \
+    GameReportEventType, GameType, SpiritScoreCategory
 
-admin.site.register(Player)
+admin.site.register(Profile)
 admin.site.register(Team)
 admin.site.register(Tournament)
 admin.site.register(TournamentParticipation)
 admin.site.register(Game)
 admin.site.register(GameReport)
 admin.site.register(SpirtScore)
+admin.site.register(Nationality)
+admin.site.register(Division)
+admin.site.register(TournamentType)
+admin.site.register(GameReportEventType)
+admin.site.register(GameType)
+admin.site.register(SpiritScoreCategory)
