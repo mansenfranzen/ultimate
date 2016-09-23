bdd9d7a1e658f9524bfcde4a2687ee942f1566de
Franz Wöllert <franz.woellert@gmail.com>
2016-09-23 23:13:29 +0200

    Add populate_db module which adds ability to pre-populate db tables using CSV files from git hub repo.

---
+++ django/app_tutorial/management/commands/populate_db.py
@@ -0,0 +1,38 @@
+"""This module is used to populate common tables."""
+
+import pandas as pd
+from django.core.management.base import BaseCommand
+from ...models import Nationality, Division, TournamentType, GameType, \
+    GameReportEventType, SpiritScoreCategory
+
+BASE_URL = "https://raw.githubusercontent.com/mansenfranzen/ultimate/" \
+              "master/material/misc/_db_populate_{class_name}.csv"
+
+
+class Command(BaseCommand):
+
+
+    def populate_from_csv(self, model):
+        """Fetch csv from github repo and populate db table.
+
+        The read_csv method requires the keep_default_na and na_values
+        parameters because it otherwise interpretes Namibias 'NA' as an np.nan
+        for example.
+        """
+
+        class_name = model.__name__
+        print("Populating {}...".format(class_name))
+        url = BASE_URL.format(class_name=class_name.lower())
+        csv = pd.read_csv(url, header=0, keep_default_na=False, na_values=[])
+
+        for idx, row in csv.iterrows():
+            model.objects.create(**row.to_dict())
+
+
+    def handle(self, *args, **options):
+        self.populate_from_csv(Nationality)
+        self.populate_from_csv(Division)
+        self.populate_from_csv(TournamentType)
+        self.populate_from_csv(GameType)
+        self.populate_from_csv(GameReportEventType)
+        self.populate_from_csv(SpiritScoreCategory)
