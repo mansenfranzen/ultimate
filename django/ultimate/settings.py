c7d3d5da2e77e09700d5b08d21b3af826b8222ff
PlusMinus0 <brandt.matthias+github@gmail.com>
2016-09-26 14:58:38 +0200

    Revert "Change DATABASES settings to MySql usage."
    This reverts commit 959aace2bfd585fd8aaae18cccc230a07cd92a67.

--- django/ultimate/settings.py
+++ django/ultimate/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'ultimate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_db',
-        'USER': 'django_user',
-        'PASSWORD': 'django_db_dummy',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
