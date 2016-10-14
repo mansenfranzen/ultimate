991f595843ee7593b69b4186334b51896e62c8e4
PlusMinus0 <brandt.matthias+github@gmail.com>
2016-10-14 17:59:54 +0200

    Updated Model (do not merge just yet).

--- django/app_tutorial/admin.py
+++ django/app_tutorial/admin.py
@@ -2,20 +2,21 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Tournament, Team, TournamentParticipation, Game, \
-    GameReport, SpirtScore, Profile, Nationality, Division, TournamentType, \
-    GameReportEventType, GameType, SpiritScoreCategory
+from .models import *
 
+admin.site.register(DivisionAge)
+admin.site.register(DivisionType)
+admin.site.register(Field)
+admin.site.register(FieldType)
+admin.site.register(Game)
+admin.site.register(GameScore)
+admin.site.register(Location)
 admin.site.register(Profile)
+admin.site.register(SpiritReport)
+admin.site.register(SpiritScore)
+admin.site.register(SpiritScoreCategory)
+admin.site.register(Squad)
 admin.site.register(Team)
 admin.site.register(Tournament)
-admin.site.register(TournamentParticipation)
-admin.site.register(Game)
-admin.site.register(GameReport)
-admin.site.register(SpirtScore)
-admin.site.register(Nationality)
-admin.site.register(Division)
-admin.site.register(TournamentType)
-admin.site.register(GameReportEventType)
-admin.site.register(GameType)
-admin.site.register(SpiritScoreCategory)
+admin.site.register(TournamentDivision)
+admin.site.register(TournamentField)
--- django/app_tutorial/models.py
+++ django/app_tutorial/models.py
@@ -4,43 +4,55 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Nationality(models.Model):
-    abbreviation = models.CharField(max_length=5)
-    name = models.CharField(max_length=75)
+# TODO (Feedback): Sort classes alphabetically or logically?
 
 
-class Division(models.Model):
+class DivisionAge(models.Model):
+    name = models.CharField(max_length=50)
+    # In our discussion this was denoted as type, I thought this might be handier.
+    age_min = models.IntegerField()
+    age_max = models.IntegerField()
+
+
+class DivisionType(models.Model):
+    # TODO (Feedback): In our discussion this was division gender, but I feel like Mixed and Open aren't really genders.
     name = models.CharField(max_length=50)
 
 
-class TournamentType(models.Model):
+class Field(models.Model):
+    location = models.CharField(max_length=100)
+    type = models.ForeignKey(FieldType)
+
+
+class FieldType(models.Model):
+    type = models.CharField(max_length=50)
+
+
+class Game(models.Model):
+    division = models.ForeignKey(TournamentDivision)  # TODO: Redundant? Contained in Squad.division
+    date = models.DateTimeField('Game Time')
+    squad_home = models.ForeignKey(Squad)
+    squad_away = models.ForeignKey(Squad)
+    # type = models.ForeignKey(GameType)
+    # round = models.IntegerField()
+
+
+class GameScore(models.Model):  # TODO (Feedback): OneToOne Relation atm --> necessary?
+    score_home = models.IntegerField()
+    score_away = models.IntegerField()
+    game = models.OneToOneField(Game)
+
+
+class Location(models.Model):
     name = models.CharField(max_length=50)
-
-
-class GameType(models.Model):
-    name = models.CharField(max_length=50)
-
-
-class GameReportEventType(models.Model):
-    name = models.CharField(max_length=50)
-
-
-class SpiritScoreCategory(models.Model):
-    name = models.CharField(max_length=50)
-
-
-class Team(models.Model):
-    name = models.CharField(max_length=50)
-    origin = models.CharField(max_length=50)
-    division = models.ForeignKey(Division)
+    location = models.CharField(max_length=100)
 
 
 class Profile(models.Model):
     user_id = models.OneToOneField(User)
-    team_id = models.ForeignKey(Team)
     number = models.IntegerField(blank=True, null=True)
     birthday = models.DateField('Birthday', blank=True)
-    nationality = models.ForeignKey(Nationality, blank=True, null=True)
+    #    nationality = models.ForeignKey(Nationality, blank=True, null=True)
     external_id_type = models.IntegerField(blank=True, null=True)
     external_id = models.IntegerField(blank=True, null=True)
 
@@ -52,47 +64,87 @@ class Profile(models.Model):
                           team=self.team_id.name)
 
 
-class Tournament(models.Model):
-    name = models.CharField(max_length=75)
-    location = models.CharField(max_length=50)
-    date_begin = models.DateField('Begin Date')
-    date_end = models.DateField('End Date')
-    type = models.ForeignKey(TournamentType)
-    division = models.ForeignKey(Division)
+# TODO (Feedback): I don't think the way we wrote it down made much sense. Is this better?
+class SpiritReport(models.Model):
+    squad_from = models.ForeignKey(Squad)
+    squad_for = models.ForeignKey(Squad)
+    game = models.ForeignKey(Game)
 
 
-class Game(models.Model):
-    tournament_id = models.ForeignKey(Tournament)
-    date = models.DateTimeField('Game Time')
-    team_id_home = models.ForeignKey(Team, related_name='team_home')
-    team_id_away = models.ForeignKey(Team, related_name='team_away')
-    score_home = models.IntegerField()
-    score_away = models.IntegerField()
-    type = models.ForeignKey(GameType)
-    round = models.IntegerField()
-
-
-class GameReport(models.Model):
-    game_id = models.ForeignKey(Game)
-    team_id = models.ForeignKey(Team)
-    player_id_assist = models.ForeignKey(Profile, blank=True, null=True,
-                                         related_name='player_assist')
-    player_id_score = models.ForeignKey(Profile, blank=True, null=True,
-                                        related_name='player_score')
-    timestamp = models.DateTimeField('Timestamp')
-    event = models.ForeignKey(GameReportEventType, blank=True, null=True)
-
-
-class SpirtScore(models.Model):
-    game_id = models.ForeignKey(Game)
-    team_id_providing = models.ForeignKey(Team, related_name='team_providing')
-    team_id_receiving = models.ForeignKey(Team, related_name='team_receiving')
-    score_category = models.ForeignKey(SpiritScoreCategory)
+class SpiritScore(models.Model):
+    report = models.ForeignKey(SpiritReport)
+    type = models.ForeignKey(SpiritScoreCategory)
     score = models.IntegerField()
 
 
-class TournamentParticipation(models.Model):
-    tournament_id = models.ForeignKey(Tournament)
-    team_id = models.ForeignKey(Team, blank=True)
-    player_id = models.ForeignKey(Profile)
-    player_number = models.IntegerField(blank=True)
+class SpiritScoreCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Squad(models.Model):
+    # TODO: When teams are added, make name nullable and build squad name from division age and type if name is empty.
+    division_age = models.ForeignKey(DivisionAge)
+    division_type = models.ForeignKey(DivisionType)
+    name = models.CharField(max_length=50)
+    # team = models.ForeignKey(Team)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    origin = models.CharField(max_length=50)
+    division = models.ForeignKey(DivisionType)
+
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=75)
+    location = models.ForeignKey(Location)
+    date_begin = models.DateField('Begin Date')
+    date_end = models.DateField('End Date')
+    # type = models.ForeignKey(TournamentType)
+
+
+class TournamentDivision(models.Model):
+    tournament = models.ForeignKey(DivisionType)
+    age_group = models.ForeignKey(DivisionAge)
+    division = models.ForeignKey(DivisionType)
+
+
+class TournamentField(models.Model):
+    tournament = models.ForeignKey(Tournament)
+    field = models.ForeignKey(Field)
+    field_number = models.IntegerField()  # TODO (Feedback): imo this is way more practical than a string name
+
+# ################### Legacy and future work
+# class Nationality(models.Model):
+#    abbreviation = models.CharField(max_length=5)
+#    name = models.CharField(max_length=75)
+#
+#
+# class TournamentType(models.Model):
+#    name = models.CharField(max_length=50)
+#
+#
+# class GameType(models.Model):
+#     name = models.CharField(max_length=50)
+#
+#
+# class GameReportEventType(models.Model):
+#     name = models.CharField(max_length=50)
+#
+#
+# class GameReport(models.Model):
+#     game_id = models.ForeignKey(Game)
+#     team_id = models.ForeignKey(Team)
+#     player_id_assist = models.ForeignKey(Profile, blank=True, null=True,
+#                                          related_name='player_assist')
+#     player_id_score = models.ForeignKey(Profile, blank=True, null=True,
+#                                         related_name='player_score')
+#     timestamp = models.DateTimeField('Timestamp')
+#     event = models.ForeignKey(GameReportEventType, blank=True, null=True)
+#
+#
+# class TournamentParticipation(models.Model):
+#     tournament_id = models.ForeignKey(Tournament)
+#     team_id = models.ForeignKey(Team, blank=True)
+#     player_id = models.ForeignKey(Profile)
+#     player_number = models.IntegerField(blank=True)
